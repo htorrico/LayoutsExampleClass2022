409a50aa9fd2fc86955e5650801d4e79ab13b3f7
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2022-01-05 21:30:59 -0500

    Agregar archivos de proyecto.

---
+++ Layouts/Layouts/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Layouts
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new StackLayoutDemo();
+            //MainPage = new GridDemo();
+            MainPage = new NavigationPage(new Page1());
+            //MainPage = new TabbedPage1();
+            //MainPage = new CarouselPage1xaml();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
